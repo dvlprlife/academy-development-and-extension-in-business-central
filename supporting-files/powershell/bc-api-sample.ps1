5bc08d287fd0aba6e9c30cb3c9e7c712eaaff02d
Brad <brad@dvlprlife.com>
2023-10-17 09:43:19 -0400

    powershell scripts

---
+++ supporting-files/powershell/bc-api-sample.ps1
@@ -0,0 +1,73 @@
+﻿
+# Get OAuth Token
+$scope = "https://api.businesscentral.dynamics.com/.default"
+
+$clientid = "your-client-id-guid"
+$clientsecret = "your-client-secret-guid"
+$environment = "Sandbox-guid"
+$tenantID = "your-tenant-id-guid"
+$company = "your-company-guid"
+
+$AuthHeader = @{
+    'Content-Type' = 'application/x-www-form-urlencoded'
+}
+
+$Body = @{
+    grant_type='client_credentials'
+    client_id=$clientid
+    client_secret=$clientsecret
+    scope=$scope
+}
+
+$Request = Invoke-RestMethod -Method POST -uri "https://login.microsoftonline.com/$($tenantid)/oauth2/v2.0/token" -Headers $AuthHeader -Body $Body
+# Build
+
+$Header = @{
+    Authorization = "$($Request.token_type) $($Request.access_token)"
+}
+
+$Req = $null
+$get = $null
+
+# Get a list of APIs
+$URL = "https://api.businesscentral.dynamics.com/v2.0/$($tenantid)/$($environment)/api/v2.0/"
+
+$Req = (Invoke-RestMethod -Method Get -Uri $URL -Headers $Header).Value
+$URL
+$Req
+
+# Get a list of Companies
+$URL = "https://api.businesscentral.dynamics.com/v2.0/$($tenantid)/$($environment)/api/v2.0/companies"
+
+$Req = (Invoke-RestMethod -Method Get -Uri $URL -Headers $Header).Value
+$URL
+$Req
+
+# get a list of Sales Orders
+$URL = "https://api.businesscentral.dynamics.com/v2.0/($tenantid)/$($environment)/api/v2.0/companies($($company))/salesOrders"
+$get = (Invoke-RestMethod -Method Get -Uri $URL -Headers $Header).Value
+$URL
+$get
+
+# Get a list of APIs
+$URL = "https://api.businesscentral.dynamics.com/v2.0/$($tenantid)/$($environment)/api/summitNA/sample/v2.0"
+$Req = (Invoke-RestMethod -Method Get -Uri $URL -Headers $Header).Value
+$URL
+$Req
+
+# Get a list of Widgets
+$Req = $null
+$URL = "https://api.businesscentral.dynamics.com/v2.0/$($tenantid)/$($environment)/api/summitNA/sample/v2.0/companies($($company))/widgets"
+$Req = (Invoke-RestMethod -Method Get -Uri $URL -Headers $Header).Value
+$URL
+$Req
+
+$Count = 1..20
+  foreach ($Num in $Count) {
+    $Num
+    $ReqBody = @{
+        "no" = "$($Num)"
+        "description" = "$($Num)"
+    } | ConvertTo-Json
+    Invoke-RestMethod -Method Post -Uri "https://api.businesscentral.dynamics.com/v2.0/$($tenantid)/$($environment)/api/SummitNA/Sample/v2.0/companies($($company))/widgets" -Headers $Header -Body $ReqBody  -ContentType "application/json"
+}
---
+++ supporting-files/powershell/bccontinerhelper.ps1
@@ -0,0 +1,37 @@
+﻿############################################################################################################
+# Set these values
+
+$containername = 'BC23'
+$version = 23
+$UserName = 'admin'
+$Password = 'password'
+$SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential ($UserName,$SecurePassword)
+
+############################################################################################################
+
+$startTime = [DateTime]::Now
+Write-Host -ForegroundColor Green "$startTime : Container load start"
+
+$artifactUrl = Get-BCArtifactUrl -version $version -country "us" -select Latest -type Sandbox
+Write-Host -ForegroundColor Yellow ($artifactUrl)
+
+New-BCContainer  `
+    -accept_eula `
+    -accept_outdated `
+    -alwaysPull `
+    -artifactUrl $artifactUrl `
+    -auth NavUserPassword `
+    -containerName $containername `
+    -Credential $Credential `
+    -includeAL `
+    -isolation hyperv `
+    -updateHosts
+
+$timespend = [Math]::Round([DateTime]::Now.Subtract($startTime).Totalseconds)
+$stopTime = [DateTime]::Now
+
+Write-Host -ForegroundColor Green "$stopTime : Container load took $timespend seconds"
+
+
+
